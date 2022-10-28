0078e491ef0bc5f99daf0a79c31935ecdf4445e8
ellenmariadev <ellenmariadev@gmail.com>
2022-10-28 20:02:28 -0300

    fix: setting port to deploy

--- src/server.js
+++ src/server.js
@@ -2,8 +2,10 @@ import express from "express";
 import cors from "cors";
 import routes from "./routes.js";
 
+const PORT = process.env.PORT || 5000
+
 const app = express();
 app.use(express.json())
 app.use(cors())
 app.use(routes);
-app.listen(5000, () => console.log("Server Up"));
+app.listen(PORT, () => console.log("Server Up"));
